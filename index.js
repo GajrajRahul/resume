44fcf1722dd8b11819f1b57e00e15a21ae89b581
Rahul Gajraj <rahulgajraj3497@gmail.com>
2022-12-21 10:10:40 +0530

    final

---
+++ index.js
@@ -0,0 +1,15 @@
+function linkedin() {
+    location.href = 'https://www.linkedin.com/in/rahul-gajraj-7ba365168/';
+}
+
+function github() {
+    location.href = "https://github.com/GajrajRahul";
+}
+
+function facebook() {
+    location.href = "https://www.facebook.com/rahul.gajraj.96";
+}
+
+function twitter() {
+    location.href = "https://twitter.com/rahulgajraj3";
+}
